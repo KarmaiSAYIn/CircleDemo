a814f41010a686ddcbbf973ca6e5aa2018c0b71f
KarmaiSAYIn <46411064+KarmaiSAYIn@users.noreply.github.com>
2019-11-07 10:05:29 -0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,33 @@
+import sys
+
+import pygame
+
+def Main():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("School")
+
+    Backgrounds = [
+        (0, 0, 0),
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+    ]
+    BackgroundIndex = 0
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    BackgroundIndex += 1
+
+                if event.key == pygame.K_DOWN:
+                    BackgroundIndex -= 1
+
+        screen.fill(Backgrounds[BackgroundIndex])
+        pygame.display.flip()
+
+Main()
